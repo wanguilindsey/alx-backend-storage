e2fcd0a9fb445f0a9321200e4ec1754b02c3063b
wanguilindsey <wanguilindsey@gmail.com>
2024-08-07 23:56:47 -0700

    users

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,13 @@
+-- 0-uniq_users.sql
+-- Script to create the 'users' table with specified attributes
+-- The 'users' table includes:
+-- - id: integer, never null, auto increment, primary key
+-- - email: string (255 characters), never null, unique
+-- - name: string (255 characters)
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    PRIMARY KEY (id)
+);
