9143659f07765516c669cdb51957de1346bc2022
wanguilindsey <wanguilindsey@gmail.com>
2024-08-08 01:05:56 -0700

    band names

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -3,12 +3,10 @@
 
 -- Query to list bands with Glam rock style and their lifespan
 SELECT band_name,
-       IFNULL(
-           CASE
-               WHEN split IS NOT NULL THEN split - formed
-               ELSE 2022 - formed
-           END, 0
-       ) AS lifespan
+       CASE
+           WHEN split IS NOT NULL THEN split - formed
+           ELSE 2022 - formed
+       END AS lifespan
 FROM metal_bands
 WHERE style = 'Glam rock'
 ORDER BY lifespan DESC;
