335aded4fb59ce515620a09e374a67edc73000ea
wanguilindsey <wanguilindsey@gmail.com>
2024-08-08 00:59:23 -0700

    country ranks

---
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -0,0 +1,8 @@
+-- 2-fans.sql
+-- SQL script to rank country origins of bands ordered by the number of (non-unique) fans
+
+-- Query to rank origins by the number of fans
+SELECT origin, SUM(fans) AS nb_fans
+FROM metal_bands
+GROUP BY origin
+ORDER BY nb_fans DESC;
