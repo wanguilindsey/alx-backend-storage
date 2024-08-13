6c85c6c1b1c3dbc69102cfbdb88661430798c7cd
wanguilindsey <wanguilindsey@gmail.com>
2024-08-13 12:20:28 -0700

    specific topic

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,2 +1,7 @@
-// Regex filter
-db.school.find({name: /^Holberton/ })
+#!/usr/bin/env python3
+""" MongoDB operations with Python using pymongo """
+
+def schools_by_topic(mongo_collection, topic):
+    """ Returns a list of schools that have a specific topic """
+    documents = mongo_collection.find({"topics": topic})
+    return list(documents)
